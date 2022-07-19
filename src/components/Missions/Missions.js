58eb3a3f8657d7912f589e180a45835575bd3d70
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-19 17:50:28 +0500

    Add styles for mission pager

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,8 +1,72 @@
 import React from 'react';
+import '../../index.css';
 
 function Missions() {
   return (
-    <div>Missions</div>
+    <>
+      <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="py-3 px-6">
+                Missions
+              </th>
+              <th scope="col" colSpan="4" className="py-3 px-6">
+                Description
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Status
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+              <th
+                scope="row"
+                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17
+              </th>
+              <td colSpan="4" className="py-4 px-6">
+                lorem34
+              </td>
+              <td className="py-4 px-6">$2999</td>
+              <td className="py-4 px-6">
+                <a
+                  href="/"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+              <th
+                scope="row"
+                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17
+              </th>
+              <td colSpan="4" className="py-4 px-6">
+                lorem34
+              </td>
+              <td className="py-4 px-6">$2999</td>
+              <td className="py-4 px-6">
+                <a
+                  href="/"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
