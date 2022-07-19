07289f51dfa7598591c8214880dddf55ba153031
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-20 04:43:53 +0500

    Fix dropDown Menu hidden

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -96,7 +96,9 @@ export default function NavBar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items
+                      className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    >
                       <NavLink to="/myprofile">
                         <Menu.Item>
                           {({ active }) => (
