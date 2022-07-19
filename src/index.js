f88c7c1ac79bfc185d63224a85ab81147db03141
lomantech <87063237+lomantech@users.noreply.github.com>
2022-07-19 14:23:27 +0300

    error config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -17,7 +16,7 @@ root.render(
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,3 @@
-
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './rocketSlice';
 
