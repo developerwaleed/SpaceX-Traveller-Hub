bb030af914bed402c43dcb6e660460bc57ddfee4
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-20 05:25:39 +0500

    Fixed active link hightlight

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -48,19 +48,14 @@ export default function NavBar() {
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <NavLink to={item.name} key={item.name}>
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-900 text-white'
-                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'px-3 py-2 rounded-md text-sm font-medium',
-                          )}
-                          aria-current={item.current ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
+                      <NavLink
+                        to={item.name}
+                        key={item.name}
+                        className={({ isActive }) => (isActive
+                          ? 'bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+                          : 'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium')}
+                      >
+                        <a href={item.href}>{item.name}</a>
                       </NavLink>
                     ))}
                   </div>
