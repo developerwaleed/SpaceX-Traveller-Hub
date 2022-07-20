69fed9d1d5bb934bd6b0d7ae17c208be279f628c
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-20 23:15:24 +0500

    Add styles

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -7,11 +7,11 @@ function Status(props) {
     <div
       className={
         reserved
-          ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xs px-2 py-1 text-center mr-2 mb-2 btn'
-          : 'text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-xs px-5 py-1 text-center mr-2 mb-2 btn'
+          ? 'text-white bg-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-900 font-medium rounded-lg text-xs px-2 py-1 text-center mr-2 mb-2 btn'
+          : 'text-white focus:ring-4 bg-teal-600 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-xs px-5 py-1 text-center mr-2 mb-2 btn'
       }
     >
-      {reserved ? ' Joined ' : 'Not Joined'}
+      {reserved ? ' Active Member ' : 'Not a Member'}
     </div>
   );
 }
