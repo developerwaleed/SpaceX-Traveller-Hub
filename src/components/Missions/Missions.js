46f8361faf4731beeaf4bb4f76b59fcb320a7d01
lomantech <87063237+lomantech@users.noreply.github.com>
2022-07-21 14:11:42 +0300

    added styling

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -3,6 +3,7 @@ import '../../index.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { reserve } from '../../redux/Missions/missions';
 import Status from '../Status/Status';
+import './Missions.css';
 
 function Missions() {
   const dispatch = useDispatch();
@@ -18,63 +19,44 @@ function Missions() {
     dispatch(reserve({ id, reserved: !reserveStatus }));
   };
   return (
-    <>
+    <div className="contain">
       <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table>
+          <thead>
             <tr>
-              <th scope="col" className="py-3 px-6 text-center">
-                Missions
-              </th>
-              <th scope="col" colSpan="4" className="py-3 px-6 text-center">
+              <th scope="col">Missions</th>
+              <th scope="col" colSpan="4">
                 Description
               </th>
-              <th scope="col" className="py-3 px-6 text-center">
-                Status
-              </th>
-              <th scope="col" className="py-3 px-7 text-center">
-                Action
-              </th>
+              <th scope="col">Status</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
             {missions.map((item) => (
-              <tr
-                className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
-                key={item.mission_id}
-              >
-                <th
-                  scope="row"
-                  className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {item.mission_name}
-                </th>
-                <td colSpan="4" className="py-4 px-6">
-                  {item.description}
-                </td>
-                <td className="py-4 px-5">
+              <tr key={item.mission_id}>
+                <th scope="row">{item.mission_name}</th>
+                <td colSpan="4">{item.description}</td>
+                <td>
                   <Status reserved={item.reserved} />
                 </td>
-                <td className="py-4 px-10">
-                  <a
-                    href="/"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
+                <td>
+                  <a href="/">
                     {item.reserved ? (
                       <button
+                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                         type="button"
                         id={item.mission_id}
-                        className="bg-transparent hover:bg-red-500  text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500  hover:border-transparent rounded text-xs"
                         onClick={(e) => handleReservation(e)}
                       >
                         Cancel Mission
                       </button>
                     ) : (
                       <button
+                        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                         type="button"
                         id={item.mission_id}
                         reserved={item.reserved}
-                        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded text-xs"
                         onClick={(e) => handleReservation(e)}
                       >
                         Join Mission
@@ -87,8 +69,7 @@ function Missions() {
           </tbody>
         </table>
       </div>
-    </>
+    </div>
   );
 }
-
 export default Missions;
