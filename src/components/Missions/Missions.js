1c5dbd90f4a830f1f60cda9c70ecc39d6c6df60b
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-21 20:36:41 +0500

    Finalized styles for mission table

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -26,22 +26,28 @@ function Missions() {
   };
   return (
     <div className="contain">
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th scope="col">Missions</th>
-              <th scope="col" colSpan="4">
+      <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr className="bg-color">
+              <th scope="col" className="py-3 px-6 text-center">
+                Missions
+              </th>
+              <th scope="col" colSpan="4" className="py-3 px-6 text-center">
                 Description
               </th>
-              <th scope="col">Status</th>
-              <th scope="col">Action</th>
+              <th scope="col" className="py-3 px-6 text-center">
+                Status
+              </th>
+              <th scope="col" className="py-3 px-7 text-center">
+                Action
+              </th>
             </tr>
           </thead>
           <tbody>
             {missions.missions.map((item) => (
-              <tr key={item.mission_id}>
-                <th scope="row">{item.mission_name}</th>
+              <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700" key={item.mission_id}>
+                <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.mission_name}</td>
                 <td colSpan="4">{item.description}</td>
                 <td>
                   <Status reserved={item.reserved} />
