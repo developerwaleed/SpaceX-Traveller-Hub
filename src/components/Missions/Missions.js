3bc8764d8ba36dcb85e60afd28bdd47eb3e27d2e
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-21 05:00:50 +0500

    Fixed status not changing

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -12,10 +12,9 @@ function Missions() {
     const { id } = e.target;
     const text = e.target.innerText;
     let reserveStatus;
-    if (text === 'Join') {
+    if (text === 'Join Mission') {
       reserveStatus = false;
-    } else if (text === 'Cancel') reserveStatus = true;
-    // dispatch(reserve(id, reserved: !reseved));
+    } else if (text === 'Cancel Mission') reserveStatus = true;
     dispatch(reserve({ id, reserved: !reserveStatus }));
   };
   return (
@@ -24,16 +23,16 @@ function Missions() {
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" className="py-3 px-6">
+              <th scope="col" className="py-3 px-6 text-center">
                 Missions
               </th>
-              <th scope="col" colSpan="4" className="py-3 px-6">
+              <th scope="col" colSpan="4" className="py-3 px-6 text-center">
                 Description
               </th>
-              <th scope="col" className="py-3 px-6">
+              <th scope="col" className="py-3 px-6 text-center">
                 Status
               </th>
-              <th scope="col" className="py-3 px-6">
+              <th scope="col" className="py-3 px-7 text-center">
                 Action
               </th>
             </tr>
@@ -53,10 +52,10 @@ function Missions() {
                 <td colSpan="4" className="py-4 px-6">
                   {item.description}
                 </td>
-                <td className="py-4 px-6">
-                  <Status />
+                <td className="py-4 px-5">
+                  <Status reserved={item.reserved} />
                 </td>
-                <td className="py-4 px-6">
+                <td className="py-4 px-10">
                   <a
                     href="/"
                     className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
@@ -65,20 +64,20 @@ function Missions() {
                       <button
                         type="button"
                         id={item.mission_id}
-                        className="bg-red-500 hover:bg-red-200 text-white font-semibold hover:text-white py-2 px-4 border  hover:border-transparent rounded text-xs"
+                        className="bg-transparent hover:bg-red-500  text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500  hover:border-transparent rounded text-xs"
                         onClick={(e) => handleReservation(e)}
                       >
-                        Cancel
+                        Cancel Mission
                       </button>
                     ) : (
                       <button
                         type="button"
                         id={item.mission_id}
                         reserved={item.reserved}
-                        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-xs"
+                        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded text-xs"
                         onClick={(e) => handleReservation(e)}
                       >
-                        Join
+                        Join Mission
                       </button>
                     )}
                   </a>
